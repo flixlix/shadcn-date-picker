a32df5489f26c00b2777f492e0fe426b8899afb3
Luca Félix <luca.ziegler.felix@gmail.com>
2025-05-04 11:58:28 +0200

    fix: :bug: paths to preview code

--- src/features/code-blocks/components/basic-date-picker.tsx
+++ src/features/code-blocks/components/basic-date-picker.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 import { fetchRepoContent } from "../lib/fetch-repo-content"
 import SyntaxHighlighter from "./syntax-highlighter"
 
-const path = "features/date-picker/date-picker.tsx"
+const path = "src/features/date-picker/date-picker.tsx"
 
 export default async function DatePickerCode() {
   const response = await fetchRepoContent(path).catch(
--- src/features/code-blocks/components/range-picker.tsx
+++ src/features/code-blocks/components/range-picker.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 import { fetchRepoContent } from "../lib/fetch-repo-content"
 import SyntaxHighlighter from "./syntax-highlighter"
 
-const path = "features/date-picker/date-range-picker.tsx"
+const path = "src/features/date-picker/date-range-picker.tsx"
 
 export default async function RangePickerCode() {
   const response = await fetchRepoContent(path).catch(
